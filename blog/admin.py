aabb155bb4ab6163b7e8f284b602c1ebf43b6b62
Shona Lowden <lowdensh@gmail.com>
2025-03-18 23:25:37 +0100

    update blog post model; update all posts html template

--- blog/admin.py
+++ blog/admin.py
@@ -1,42 +1,60 @@
 # https://testdriven.io/blog/django-custom-user-model/
 
-from .models import Post
+from .models import Category, Tag, Post
 from django.contrib import admin
+
+
+@admin.register(Category)
+class PostAdmin(admin.ModelAdmin):
+  # Main list
+  list_display = ('name', )
+  list_display_links = ('name', )
+  search_fields = ('name', )
+
+
+@admin.register(Tag)
+class PostAdmin(admin.ModelAdmin):
+  # Main list
+  list_display = ('name', )
+  list_display_links = ('name', )
+  search_fields = ('name', )
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    # Main list
-    list_display = (
-        'short_title',
-        'author_display_name',
-        'date_published',
-        'date_edited',
-    )
-    list_display_links = ('short_title', )
-    list_filter = ('author__email', )
-    search_fields = (
-        'title',
-        'author__first_name',
-        'author__last_name',
-        'author__email',
-        'content',
-    )
+  # Main list
+  list_display = (
+    'short_title',
+    'author_display_name',
+    'date_published',
+    'category',
+  )
+  list_display_links = ('short_title', )
+  list_filter = ('author__email', )
+  search_fields = (
+    'title',
+    'author__first_name',
+    'author__last_name',
+    'author__email',
+    'content',
+  )
 
-    # Specific Post instance
-    fieldsets = (
-        ('Meta', {'fields': (
-            'author',
-            'date_published',
-            'date_edited',
-        )}),
-        ('Details', {'fields': (
-            'title',
-            'content',
-        )}),
-    )
-    readonly_fields = ['date_published', 'date_edited', ]
+  # Specific Post instance
+  fieldsets = (
+    ('Meta', {'fields': (
+      'author',
+      'date_published',
+      'date_edited',
+      'category',
+      'tags',
+    )}),
+    ('Details', {'fields': (
+      'title',
+      'content',
+    )}),
+  )
+  readonly_fields = ['date_published', 'date_edited', ]
 
-    # Additional or renamed attributes
-    def author_display_name(self, obj):
-        return obj.author.display_name
+  # Additional or renamed attributes
+  def author_display_name(self, obj):
+    return obj.author.display_name
--- blog/models.py
+++ blog/models.py
@@ -1,44 +1,120 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from users.models import CustomUser
 
 
 def format_datetime(datetime):
-    return datetime.strftime('%Y-%m-%d %H:%M:%S')
+  return datetime.strftime('%Y-%m-%d %H:%M:%S')
+
+
+class Category(models.Model):
+  name = models.CharField(
+    max_length=32,
+    unique=True,
+  )
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    ordering = ['name', ]
+    verbose_name = _('category')
+    verbose_name_plural = _('categories')
+
+
+class Tag(models.Model):
+  name = models.CharField(
+    max_length=16,
+    unique=True,
+  )
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    ordering = ['name', ]
+    verbose_name = _('tag')
+    verbose_name_plural = _('tags')
 
 
 class Post(models.Model):
-    title = models.CharField(
-        max_length=80,
-        default='Untitled blog post'
-    )
-    author = models.ForeignKey(
-        to=CustomUser,
-        on_delete=models.CASCADE,
-        related_name='posts'
-    )
-    date_published = models.DateTimeField(auto_now_add=True)
-    date_edited = models.DateTimeField(
-        auto_now=True,
-        null=True,
-    )
-    content = models.TextField(
-        max_length=2056,
-        default='There should be some post content here, but I was empty and left as the default :('
+  title = models.CharField(
+    max_length=80,
+    default='Untitled blog post'
+  )
+  author = models.ForeignKey(
+    to=CustomUser,
+    on_delete=models.CASCADE,
+    related_name='posts'
+  )
+  date_published = models.DateTimeField(
+    auto_now_add=True
+  )
+  date_edited = models.DateTimeField(
+    auto_now=True,
+    null=True,
+  )
+  category = models.ForeignKey(
+    to=Category,
+    blank=True,
+    null=True,
+    on_delete=models.SET_NULL,
+    related_name='posts',
+  )
+  tags = models.ManyToManyField(
+    to=Tag,
+    blank=True,
+    related_name='tags',
+  )
+  content = models.TextField(
+    default='There should be some post content here, but I was empty and left as the default :('
+  )
+
+  @property
+  def short_title(self):
+    max_chars = 39
+    if len(self.title) > max_chars:
+      return self.title[:max_chars] + '...'
+    return self.title
+
+  def short_content(self):
+    max_chars = 320
+    if len(self.content) > max_chars:
+      return self.content[:max_chars] + '... (continued)'
+    return self.content
+
+  @property
+  def tags_count(self):
+    return self.tags.count()
+
+  @property
+  def list_of_tags(self):
+    return self.tags.all()
+
+  @property
+  def string_of_tags(self):
+    if self.tags_count == 0:
+      return 'none! (yet?)'
+
+    list_of_tags = self.list_of_tags
+    string = list_of_tags[0].name
+
+    if self.tags_count == 1:
+      return string
+
+    for tag in list_of_tags[1:]:
+      string += f', {tag.name}'
+
+    return string
+
+  def __str__(self):
+    return (
+      f'"{self.short_title}" '
+      f'by: {self.author} '
+      f'on: {format_datetime(self.date_published)}'
     )
 
-    @property
-    def short_title(self):
-        max_chars = 29
-        if len(self.title) > max_chars:
-            return self.title[:max_chars] + '...'
-        return self.title
-
-    def __str__(self):
-        return (
-            f'"{self.short_title}" '
-            f'by {self.author} '
-            f'on {format_datetime(self.date_published)}'
-        )
-
-    class Meta:
-        ordering = ['-date_published', 'author', ]
+  class Meta:
+    ordering = ['-date_published', 'author', ]
+    verbose_name = _('post')
+    verbose_name_plural = _('posties')
