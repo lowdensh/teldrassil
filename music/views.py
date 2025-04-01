d4ed3c108a17ed4329a9a3a4222397f4ab7dff38
Shona Lowden <lowdensh@gmail.com>
2025-04-01 13:19:36 +0200

    add songs manually to music page
    TODO: models? web scraping?

--- music/views.py
+++ music/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+# from bs4 import BeautifulSoup
+# from urllib.request import urlopen
 
 
 def music_tracks_all(request):
+  # url = "https://soundcloud.com/shona-lowden/tracks"
+  # page = urlopen(url)
+  # html = page.read().decode("utf-8")
+  # soup = BeautifulSoup(html, "html.parser")
+  # list = soup.find_all("li", class_="soundList__item")
+
   context = {}
   return render(request, 'music/tracks_summary.html', context)
